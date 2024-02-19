33943a1d2afd976ce7a3fe0f3ec0259c57585316
ltuffery <123221865+ltuffery@users.noreply.github.com>
2024-02-19 17:03:39 +0100

    finish fix test ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -28,27 +28,27 @@ int main()
 		std::stack<int> s(mstack);
 	}
 	{
-	MutantStack<std::string> mstack;
-	mstack.push(5);
-	mstack.push(17);
-	std::cout << mstack.top() << std::endl;
-	mstack.pop();
-	std::cout << mstack.size() << std::endl;
-	mstack.push(3);
-	mstack.push(5);
-	mstack.push(737);
-	//[...]
-	mstack.push(0);
-	MutantStack<int>::iterator it = mstack.begin();
-	MutantStack<int>::iterator ite = mstack.end();
-	++it;
-	--it;
-	while (it != ite)
-	{
-	std::cout << *it << std::endl;
-	++it;
-	}
-	std::stack<int> s(mstack);
+		MutantStack<std::string> mstack;
+		mstack.push("Hey");
+		mstack.push("Hello");
+		std::cout << mstack.top() << std::endl;
+		mstack.pop();
+		std::cout << mstack.size() << std::endl;
+		mstack.push("bla...");
+		mstack.push("Yes");
+		mstack.push("42");
+		//[...]
+		mstack.push("no...");
+		MutantStack<std::string>::iterator it = mstack.begin();
+		MutantStack<std::string>::iterator ite = mstack.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+		std::cout << *it << std::endl;
+		++it;
+		}
+		std::stack<std::string> s(mstack);
 	}
 	return 0;
 }
