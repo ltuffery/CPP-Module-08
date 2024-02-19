37cdd05a1ea34a03873cb0ac7d94ba949e815a52
ltuffery <123221865+ltuffery@users.noreply.github.com>
2024-02-19 16:54:36 +0100

    finish ex02

---
+++ ex02/MutantStack.hpp
@@ -0,0 +1,20 @@
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+#include <iterator>
+#include <stack>
+
+template<typename T>
+class MutantStack : public std::stack<T>
+{
+	public:
+		typedef typename std::stack<T>::container_type::iterator iterator;
+		iterator begin() {
+			return this->c.begin();
+		}
+		iterator end() {
+			return this->c.end();
+		}
+};
+
+#endif
