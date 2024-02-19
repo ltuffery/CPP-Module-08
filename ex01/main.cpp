cd9cb8a584373bdaa5b2d65be387f14e05bf923c
ltuffery <123221865+ltuffery@users.noreply.github.com>
2024-02-19 16:54:19 +0100

    add test ex01

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,4 +1,5 @@
 #include "Span.hpp"
+#include <exception>
 #include <iostream>
 
 int main()
@@ -10,6 +11,19 @@ int main()
 		sp.addNumber(17);
 		sp.addNumber(9);
 		sp.addNumber(11);
+		std::cout << sp.shortestSpan() << std::endl;
+		std::cout << sp.longestSpan() << std::endl;
+
+		try {
+			sp.addNumber(8);
+		} catch(std::exception e) {
+			std::cout << e.what() << std::endl;
+		}
+	}
+	{
+		Span sp = Span(5);
+
+		sp.addNumbers(1, 4);
 		std::cout << sp.shortestSpan() << std::endl;
 		std::cout << sp.longestSpan() << std::endl;
 	}
